c60ae40808c92f8a4779f79e2170a0ad020df95b
Nik Malhotra <nmal@me.com>
2019-02-26 20:05:45 +0000

    added foreign key successfully

--- migrations/20190226194503_step2.js
+++ migrations/20190226194503_step2.js
@@ -1,8 +1,20 @@
+exports.up = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('milestones'),
+    knex.schema.createTable('milestones', table => {
+      table.increments('id');
+      table.integer('famous_person_id');
+      table.string('description');
+      table.date('date_achieved');
+      table.foreign('famous_person_id').references('id').inTable('famous_people');
 
-exports.up = function(knex, Promise) {
-  
+    })
+  ])
+
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('milestones')
+  ]);
 };
